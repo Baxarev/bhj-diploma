6c37378c0824da6cc84c5c74d953c40a6f190473
Baxarev <baxarev.ilya@mail.ru>
2023-07-01 12:41:32 +0300

    1 API

--- public/js/api/User.js
+++ public/js/api/User.js
@@ -4,12 +4,13 @@
  * Имеет свойство URL, равное '/user'.
  * */
 class User {
+  static URL = '/user';
   /**
    * Устанавливает текущего пользователя в
    * локальном хранилище.
    * */
   static setCurrent(user) {
-
+    localStorage.setItem('user', JSON.stringify(user));
   }
 
   /**
@@ -17,7 +18,7 @@ class User {
    * пользователе из локального хранилища.
    * */
   static unsetCurrent() {
-
+    localStorage.removeItem('user');
   }
 
   /**
@@ -25,7 +26,7 @@ class User {
    * из локального хранилища
    * */
   static current() {
-
+    return JSON.parse(localStorage.getItem('user')) ?? undefined;
   }
 
   /**
@@ -33,7 +34,20 @@ class User {
    * авторизованном пользователе.
    * */
   static fetch(callback) {
-
+    createRequest({
+      url: this.URL + '/current',
+      method: 'GET',
+      callback: (err, response) => {
+        if (!response.success) {
+          this.unsetCurrent();
+          callback(null, response);
+        }
+        if (response.success) {
+          this.setCurrent(response.user)
+          callback(null, response);
+        }
+      }
+    });
   }
 
   /**
@@ -64,7 +78,17 @@ class User {
    * User.setCurrent.
    * */
   static register(data, callback) {
-
+    createRequest({
+      url: this.URL + '/register',
+      method: 'POST',
+      data,
+      callback: (err, response) => {
+        if (response.success) {
+          this.setCurrent(response.user)
+        }
+        callback(err, response);
+      }
+    })
   }
 
   /**
@@ -72,6 +96,10 @@ class User {
    * выхода необходимо вызвать метод User.unsetCurrent
    * */
   static logout(callback) {
-
+    createRequest({
+      url: this.URL + '/logout',
+      method: 'POST',
+      callback,
+    })
   }
 }
--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,5 +3,20 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
+  const xhr = new XMLHttpRequest();
+  xhr.responseType = 'json';
+  xhr.open(options.method, options.url)
 
+  if (options.method === 'GET') {
+    xhr.send();
+  } else {
+    const formData = new FormData
+    for (key in options.data) {
+      formData.append(key, options.data[key])
+    }
+    xhr.send(formData);
+  }
+
+  xhr.addEventListener('load', () => options.callback(null, xhr.response))
+  xhr.addEventListener('error', (e) => options.callback(e.type, null))
 };
